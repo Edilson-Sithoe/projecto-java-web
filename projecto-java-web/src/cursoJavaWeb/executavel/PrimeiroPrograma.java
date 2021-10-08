ac065564a7f70cb8591e70881966ba949b691567
Edilson Sithoe <54285895+Edilson-Sithoe@users.noreply.github.com>
2021-10-08 14:21:44 +0200

    Commit dois

--- projecto-java-web/src/cursoJavaWeb/executavel/PrimeiroPrograma.java
+++ projecto-java-web/src/cursoJavaWeb/executavel/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 package cursoJavaWeb.executavel;
 
 public class PrimeiroPrograma {
+	/*Main eh um metodo de auto execucao do java*/
 	public static void main(String[] args) {
 		System.out.println("Ola Mundo com GitHub!");
 	}

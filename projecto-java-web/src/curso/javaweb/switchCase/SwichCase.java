e354fba3c6c0935893b1b5f38f7feafcc1698e65
Edilson-Sithoe <54285895+Edilson-Sithoe@users.noreply.github.com>
2021-10-19 11:45:46 +0200

    Add novo package

---
+++ projecto-java-web/src/curso/javaweb/switchCase/SwichCase.java
@@ -0,0 +1,30 @@
+package curso.javaweb.switchCase;
+
+import javax.swing.DefaultBoundedRangeModel;
+
+public class SwichCase {
+	public static void main(String[] args) {
+		int nota1 = 10;
+		int nota2 = 12;
+		int nota3 = 14;
+		int nota4 = 16;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		int dia = 2;
+		switch (dia) {
+			case 1:
+				System.out.println("Domingo");
+			break;
+			case 2:
+				System.out.println("Segunda-feira");
+			break;
+			case 3:
+				System.out.println("Terça-feria");
+			break;
+			default: 
+				System.out.println("Outro valor " + dia);
+			break;
+		}
+	}
+}

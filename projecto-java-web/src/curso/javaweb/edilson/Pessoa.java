617485518153681f30ea1e385310af2bd7238a2a
Edilson-Sithoe <54285895+Edilson-Sithoe@users.noreply.github.com>
2021-10-19 09:22:01 +0200

    Add campo data de nascimento

--- projecto-java-web/src/curso/javaweb/edilson/Pessoa.java
+++ projecto-java-web/src/curso/javaweb/edilson/Pessoa.java
@@ -1,28 +1,43 @@
 package curso.javaweb.edilson;
 
+import java.util.Date;
+
 public class Pessoa {
 	private String nome;
 	private String apelido;
 	private int idade;
-	
+	private Date data_nascimento;
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getApelido() {
 		return apelido;
 	}
+
 	public void setApelido(String apelido) {
 		this.apelido = apelido;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-	
-	
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
 }

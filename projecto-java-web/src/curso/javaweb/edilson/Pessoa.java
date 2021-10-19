522c9f8aa9303ddc2f3196da6df279ae66b8234b
Edilson-Sithoe <54285895+Edilson-Sithoe@users.noreply.github.com>
2021-10-19 09:05:43 +0200

    Add campo na classe Pessoa

--- projecto-java-web/src/curso/javaweb/edilson/Pessoa.java
+++ projecto-java-web/src/curso/javaweb/edilson/Pessoa.java
@@ -1,5 +1,28 @@
 package curso.javaweb.edilson;
 
 public class Pessoa {
-
+	private String nome;
+	private String apelido;
+	private int idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getApelido() {
+		return apelido;
+	}
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
 }

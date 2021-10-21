c41805e4fd520737e7e6e7d56013a38263bf6645
Edilson-Sithoe <54285895+Edilson-Sithoe@users.noreply.github.com>
2021-10-21 09:03:46 +0200

    Commit 21102021

---
+++ projecto-java-web/src/curso/javaweb/ciclofor/CicloFor.java
@@ -0,0 +1,11 @@
+package curso.javaweb.ciclofor;
+
+public class CicloFor {
+	public static void min(String[] args) {
+		
+		for (int numero = 10; numero < 10; numero--) {
+			System.out.println("numero");
+		}
+	}
+
+}

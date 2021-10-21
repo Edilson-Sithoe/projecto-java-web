918cf141e57d15b41e06ad158d9c4c3295433359
Edilson-Sithoe <54285895+Edilson-Sithoe@users.noreply.github.com>
2021-10-21 12:19:54 +0200

    commit getters and setters

--- projecto-java-web/src/cursoJavaWeb/classes/Aluno.java
+++ projecto-java-web/src/cursoJavaWeb/classes/Aluno.java
@@ -2,15 +2,63 @@ package cursoJavaWeb.classes;
 
 public class Aluno {
 	
-	public String nome;
-	public String apelido;
-	int idade;
-	String dataNasciemtno;
-	String morada;
-	String dataMatricula;
+	private String nome;
+	private String apelido;
+	private int idade;
+	private String dataNascimento;
+	private String morada;
+	private String dataMatricula;
 	
 	public Aluno() {
 		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getdataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setdataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getMorada() {
+		return morada;
+	}
+
+	public void setMorada(String morada) {
+		this.morada = morada;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
 	}
 	
 	
--- projecto-java-web/src/cursoJavaWeb/executavel/PrimeiroPrograma.java
+++ projecto-java-web/src/cursoJavaWeb/executavel/PrimeiroPrograma.java
@@ -9,12 +9,36 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno();
-		aluno1.nome = "Edilson";
-		aluno1.apelido = "Sithoe";
-		System.out.println("Nome: "+aluno1.nome+" "+aluno1.apelido);
-		JOptionPane.showMessageDialog(null,"Nome completo: "+ aluno1.nome+" "+aluno1.apelido);
+		aluno1.setNome("Edilson");
+		aluno1.setApelido("Sithoe");
+		aluno1.setIdade(26);
+		aluno1.setdataNascimento("05-10-1995");
+		aluno1.setMorada("Zimpeto");
+		aluno1.setDataMatricula("22-10-2021");
+		
+		System.out.println("Nome: "+aluno1.getNome()+";");
+		System.out.println("Apelido: "+aluno1.getApelido()+";");
+		System.out.println("Idade: "+aluno1.getIdade()+";");
+		System.out.println("Data de Nasciemnto: "+aluno1.getdataNascimento()+";");
+		System.out.println("Morada: "+aluno1.getMorada()+";");
+		System.out.println("Data da Matricula: "+aluno1.getDataMatricula()+".");
 		
 		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Yannick");
+		aluno2.setApelido("Mahumane");
+		aluno2.setIdade(26);
+		aluno2.setdataNascimento("05-10-1995");
+		aluno2.setMorada("Zimpeto");
+		aluno2.setDataMatricula("22-10-2021");
+		
+		System.out.println("********************** Aluno 2 *****************************");
+		System.out.println("Nome: "+aluno2.getNome()+";");
+		System.out.println("Apelido: "+aluno2.getApelido()+";");
+		System.out.println("Idade: "+aluno2.getIdade()+";");
+		System.out.println("Data de Nasciemnto: "+aluno2.getdataNascimento()+";");
+		System.out.println("Morada: "+aluno2.getMorada()+";");
+		System.out.println("Data da Matricula: "+aluno2.getDataMatricula()+".");
+		
 		Aluno aluno3 = new Aluno();
 	}
 }
